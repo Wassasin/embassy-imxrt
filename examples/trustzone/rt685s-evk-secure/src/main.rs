2425f60166eedbabc5bbe14de2d1c961cea0c26d
Dion Dokter <dion@tweedegolf.com>
2025-09-29 16:27:26 +0200

    Add trustzone project

---
+++ examples/trustzone/rt685s-evk-secure/src/main.rs
@@ -0,0 +1,119 @@
+#![no_std]
+#![no_main]
+#![feature(abi_cmse_nonsecure_call)]
+#![feature(cmse_nonsecure_entry)]
+
+use core::panic::PanicInfo;
+use core::u32;
+
+use cortex_m::peripheral::sau::SauRegion;
+use mimxrt600_fcb::FlexSPIFlashConfigurationBlock;
+
+// auto-generated version information from Cargo.toml
+include!(concat!(env!("OUT_DIR"), "/biv.rs"));
+
+#[link_section = ".otfad"]
+#[used]
+static OTFAD: [u8; 256] = [0; 256];
+
+#[rustfmt::skip]
+#[link_section = ".fcb"]
+#[used]
+static FCB: FlexSPIFlashConfigurationBlock = FlexSPIFlashConfigurationBlock::build();
+
+#[link_section = ".keystore"]
+#[used]
+static KEYSTORE: [u8; 2048] = [0; 2048];
+
+const SECURE_START_FLASH: u32 = 0x08001000;
+const NONSECURE_START_FLASH: *const [u32; 2] = 0x08006000 as *const [u32; 2];
+const SECURE_START_RAM: u32 = 0x20080000;
+const NONSECURE_START_RAM: u32 = 0x20081000;
+
+const VTOR_NS: *mut u32 = 0xE002ED08 as *mut u32;
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    let mut cp = cortex_m::Peripherals::take().unwrap();
+
+    unsafe {
+        let [nonsecure_sp, nonsecure_reset] = NONSECURE_START_FLASH.read_volatile();
+
+        if nonsecure_sp == u32::MAX || nonsecure_reset == u32::MAX {
+            loop {
+                cortex_m::asm::nop();
+            }
+        }
+
+        // Set all regions not used by this program to non-secure
+        // Before flash
+        cp.SAU
+            .set_region(
+                0,
+                SauRegion {
+                    base_address: 0,
+                    limit_address: SECURE_START_FLASH - 1,
+                    attribute: cortex_m::peripheral::sau::SauRegionAttribute::NonSecure,
+                },
+            )
+            .unwrap();
+        cp.SAU
+            .set_region(
+                1,
+                SauRegion {
+                    base_address: NONSECURE_START_FLASH as u32,
+                    limit_address: SECURE_START_RAM - 1,
+                    attribute: cortex_m::peripheral::sau::SauRegionAttribute::NonSecure,
+                },
+            )
+            .unwrap();
+        cp.SAU
+            .set_region(
+                2,
+                SauRegion {
+                    base_address: NONSECURE_START_RAM,
+                    limit_address: u32::MAX,
+                    attribute: cortex_m::peripheral::sau::SauRegionAttribute::NonSecure,
+                },
+            )
+            .unwrap();
+        cp.SAU.enable();
+
+        // Set the nonsecure VTOR
+        VTOR_NS.write_volatile(NONSECURE_START_FLASH as u32);
+
+        // Set all interrupts to non-secure
+        for itns in &cp.NVIC.itns {
+            itns.write(u32::MAX);
+        }
+
+        // Set the non-secure stack pointer
+        cortex_m::register::msp::write_ns(nonsecure_sp);
+
+        // Create the right function pointer to the reset vector
+        let nonsecure_reset =
+            core::mem::transmute::<*const u32, extern "cmse-nonsecure-call" fn()>(nonsecure_reset as *const u32);
+        // Jump
+        nonsecure_reset();
+
+        cortex_m::asm::udf();
+    }
+}
+
+#[cortex_m_rt::exception(trampoline = false)]
+unsafe fn HardFault() -> ! {
+    loop {
+        cortex_m::asm::nop();
+    }
+}
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    cortex_m::asm::udf();
+}
+
+#[unsafe(no_mangle)]
+pub extern "cmse-nonsecure-entry" fn do_stuff_secure(num: u32) -> u32 {
+    num * 2
+}
+

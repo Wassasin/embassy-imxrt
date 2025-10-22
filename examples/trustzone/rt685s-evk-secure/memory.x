63ef901c0b8f454fb5860c51faa10b113f30f6a7
Wouter Geraedts <git@woutergeraedts.nl>
2025-10-22 09:56:28 +0200

    WIP

--- examples/trustzone/rt685s-evk-secure/memory.x
+++ examples/trustzone/rt685s-evk-secure/memory.x
@@ -1,39 +1,11 @@
 MEMORY {
-    OTFAD     : ORIGIN = 0x08000000, LENGTH = 256,
-    FCB       : ORIGIN = 0x08000400, LENGTH = 512,
-    BIV       : ORIGIN = 0x08000600, LENGTH = 4,
-    KEYSTORE  : ORIGIN = 0x08000800, LENGTH = 2K,
-    FLASH     : ORIGIN = 0x08001000, LENGTH = 20k,
-    RAM       : ORIGIN = 0x20000000, LENGTH = 4K,
-    SHAREDRTT : ORIGIN = 0x20001000, LENGTH = 2K
+    FLASH     : ORIGIN = 0x10170000, LENGTH = 20k,
+    RAM       : ORIGIN = 0x30176000, LENGTH = 8K,
+    SHAREDRTT : ORIGIN = 0x30178000, LENGTH = 2K
 }
 
 SECTIONS {
-    .otfad : {
-        . = ALIGN(4);
-        KEEP(* (.otfad))
-        . = ALIGN(4);
-    } > OTFAD
-
-    .fcb : {
-        . = ALIGN(4);
-        KEEP(* (.fcb))
-        . = ALIGN(4);
-    } > FCB
-
-    .biv : {
-        . = ALIGN(4);
-        KEEP(* (.biv))
-        . = ALIGN(4);
-    } > BIV
-
-    .keystore : {
-        . = ALIGN(4);
-        KEEP(* (.keystore))
-        . = ALIGN(4);
-    } > KEYSTORE
-
-    .shared_rtt : {
+    .shared_rtt ORIGIN(SHAREDRTT) (NOLOAD): {
         . = ALIGN(4);
         KEEP(* (.shared_rtt.header))
         KEEP(* (.shared_rtt.buffer))
--- examples/trustzone/rt685s-evk-secure/src/main.rs
+++ examples/trustzone/rt685s-evk-secure/src/main.rs
@@ -9,9 +9,9 @@ use core::sync::atomic::AtomicU32;
 use core::u32;
 
 use cortex_m::peripheral::sau::SauRegion;
-use mimxrt600_fcb::FlexSPIFlashConfigurationBlock;
+use cortex_m::peripheral::{NVIC, SCB};
 use mimxrt685s_pac::ahb_secure_ctrl::ram00_rule::Rule0;
-use mimxrt685s_pac::{ahb_secure_ctrl, AhbSecureCtrl};
+use mimxrt685s_pac::{ahb_secure_ctrl, interrupt, AhbSecureCtrl, Interrupt, ScnScb};
 use rtt_target::rprintln;
 use rtt_target::ChannelMode::NoBlockSkip;
 
@@ -22,25 +22,22 @@ include!(concat!(env!("OUT_DIR"), "/biv.rs"));
 #[used]
 static OTFAD: [u8; 256] = [0; 256];
 
-#[rustfmt::skip]
-#[link_section = ".fcb"]
-#[used]
-static FCB: FlexSPIFlashConfigurationBlock = FlexSPIFlashConfigurationBlock::build();
-
 #[link_section = ".keystore"]
 #[used]
 static KEYSTORE: [u8; 2048] = [0; 2048];
 
-const SECURE_START_FLASH: u32 = 0x08001000;
+const SECURE_START_FLASH: u32 = 0x10170000;
 const NONSECURE_START_FLASH: *const [u32; 2] = 0x08006000 as *const [u32; 2];
 const SECURE_START_RAM: u32 = 0x20000000;
 const NONSECURE_START_RAM: u32 = 0x20001000;
+const NONSECURE_START_PERIPHERALS: u32 = 0x40000000;
 
 extern "Rust" {
     static __veneer_base: ();
     static __veneer_limit: ();
 }
 
+const VTOR_S: *mut u32 = 0xE002ED08 as *mut u32;
 const VTOR_NS: *mut u32 = 0xE002ED08 as *mut u32;
 
 #[cortex_m_rt::entry]
@@ -72,12 +69,46 @@ fn main() -> ! {
             }
         }
 
+        let ahb_secure_ctrl = &*AhbSecureCtrl::ptr().add(0x1000_0000);
+
+        // rprintln!("Configuring secure_violation_irq");
+        ahb_secure_ctrl.misc_ctrl_reg().modify(|_, w| {
+            w.disable_violation_abort()
+                .enable()
+                .idau_all_ns()
+                .disable()
+                .enable_ns_priv_check()
+                .disable()
+                .enable_s_priv_check()
+                .disable()
+            // .disable_simple_master_strict_mode()
+            // .tier_mode()
+            // .disable_smart_master_strict_mode()
+            // .tier_mode()
+        });
+        ahb_secure_ctrl.misc_ctrl_dp_reg().modify(|_, w| {
+            w.disable_violation_abort()
+                .enable()
+                .idau_all_ns()
+                .disable()
+                .enable_ns_priv_check()
+                .disable()
+                .enable_s_priv_check()
+                .disable()
+            // .disable_simple_master_strict_mode()
+            // .tier_mode()
+            // .disable_smart_master_strict_mode()
+            // .tier_mode()
+        });
+
         rprintln!("Setting up regions");
         rprintln!(
             "Veneers: {:#010X} .. {:#010X}",
             &raw const __veneer_base as u32,
             &raw const __veneer_limit as u32
         );
+
+        NVIC::unmask(Interrupt::SECUREVIOLATION);
 
         // Make sure all writes and reads are done before changing the security settings
         cortex_m::asm::dsb();
@@ -90,16 +121,6 @@ fn main() -> ! {
             .set_region(
                 0,
                 SauRegion {
-                    base_address: 0,
-                    limit_address: SECURE_START_FLASH - 1,
-                    attribute: cortex_m::peripheral::sau::SauRegionAttribute::NonSecure,
-                },
-            )
-            .unwrap();
-        cp.SAU
-            .set_region(
-                1,
-                SauRegion {
                     base_address: &raw const __veneer_base as u32,
                     limit_address: &raw const __veneer_limit as u32 - 1,
                     attribute: cortex_m::peripheral::sau::SauRegionAttribute::NonSecureCallable,
@@ -108,11 +129,11 @@ fn main() -> ! {
             .unwrap();
         cp.SAU
             .set_region(
-                2,
+                1,
                 SauRegion {
-                    base_address: NONSECURE_START_FLASH as u32,
-                    limit_address: SECURE_START_RAM - 1,
-                    attribute: cortex_m::peripheral::sau::SauRegionAttribute::NonSecure,
+                    base_address: 0x10170000,
+                    limit_address: 0x10176000 - 1,
+                    attribute: cortex_m::peripheral::sau::SauRegionAttribute::Secure,
                 },
             )
             .unwrap();
@@ -120,17 +141,20 @@ fn main() -> ! {
             .set_region(
                 3,
                 SauRegion {
-                    base_address: NONSECURE_START_RAM,
-                    limit_address: u32::MAX,
-                    attribute: cortex_m::peripheral::sau::SauRegionAttribute::NonSecure,
+                    base_address: 0x2100_0000,
+                    limit_address: 0x2200_0000 - 1,
+                    attribute: cortex_m::peripheral::sau::SauRegionAttribute::NonSecureCallable,
                 },
             )
             .unwrap();
+
+        cortex_m::asm::dsb();
+        cortex_m::asm::isb();
+
         cp.SAU.enable();
 
         // Then set the ROM to secure only
         rprintln!("Set ROM to secure");
-        let ahb_secure_ctrl = &*AhbSecureCtrl::ptr().add(0x1000_0000);
         for rom_mem in ahb_secure_ctrl.rom_mem_rule_iter() {
             rom_mem.write(|w| {
                 w.rule0().secure_nonpriv_user_allowed();
@@ -146,9 +170,18 @@ fn main() -> ! {
         // Set the secure RAM to secure only
         rprintln!("Set RAM to secure");
         set_ram_secure(SECURE_START_RAM..NONSECURE_START_RAM, ahb_secure_ctrl);
+        rtt_target::UpChannel::conjure(0).unwrap().flush();
+
+        rprintln!("Set ROM range to secure");
+        set_ram_secure(0x2014_0000..0x2017_0000, ahb_secure_ctrl);
+        rtt_target::UpChannel::conjure(0).unwrap().flush();
+
+        rprintln!("Set secure RAM for code and data to secure");
+        set_ram_secure(0x2017_0000..0x2018_0000, ahb_secure_ctrl);
+        rtt_target::UpChannel::conjure(0).unwrap().flush();
 
         rprintln!("Setting all bus masters to non-secure");
-        ahb_secure_ctrl.master_sec_level().write(|w| {
+        ahb_secure_ctrl.master_sec_level().modify(|_, w| {
             w.dma0_sec()
                 .enum_ns_np()
                 .dma1_sec()
@@ -164,13 +197,13 @@ fn main() -> ! {
                 .master_sec_level_lock()
                 .writable()
         });
-        ahb_secure_ctrl.master_sec_level_anti_pol().write(|w| {
+        ahb_secure_ctrl.master_sec_level_anti_pol().modify(|_, w| {
             w.dma0_sec()
                 .enum_ns_np()
                 .dma1_sec()
                 .enum_ns_np()
                 .dsp_sec()
-                .enum_s_p()
+                .enum_ns_np()
                 .powerquad_sec()
                 .enum_ns_np()
                 .sdio0_sec()
@@ -186,29 +219,13 @@ fn main() -> ! {
             ahb_secure_ctrl.master_sec_level_anti_pol().read().bits()
         );
 
-        rprintln!("Enabling AHB bus checks");
-        ahb_secure_ctrl.misc_ctrl_reg().write(|w| {
-            w.enable_ns_priv_check()
-                .enable()
-                .enable_secure_checking()
-                .enable()
-                .enable_s_priv_check()
-                .enable()
-        });
-        ahb_secure_ctrl.misc_ctrl_dp_reg().write(|w| {
-            w.enable_ns_priv_check()
-                .enable()
-                .enable_secure_checking()
-                .enable()
-                .enable_s_priv_check()
-                .enable()
-        });
-
         rprintln!(
             "misc_ctrl_reg: {:#010X}, {:#010X}",
             ahb_secure_ctrl.misc_ctrl_reg().read().bits(),
             ahb_secure_ctrl.misc_ctrl_dp_reg().read().bits()
         );
+
+        rtt_target::UpChannel::conjure(0).unwrap().flush();
 
         // Make sure the new settings take effect immediately:
         // https://developer.arm.com/documentation/100235/0100/The-Cortex-M33-Peripherals/Security-Attribution-and--Memory-Protection/Updating-protected-memory-regions
@@ -216,17 +233,108 @@ fn main() -> ! {
         cortex_m::asm::isb();
 
         // Set the nonsecure VTOR
-        VTOR_NS.write_volatile(NONSECURE_START_FLASH as u32);
+        // VTOR_NS.write_volatile(NONSECURE_START_FLASH as u32);
+        VTOR_NS.write_volatile(SECURE_START_FLASH as u32);
+        cp.SCB.vtor.write(SECURE_START_FLASH as u32);
 
         // Set all interrupts to non-secure
         for itns in &cp.NVIC.itns {
             itns.write(u32::MAX);
+            // itns.write(0);
         }
 
         // Set the non-secure stack pointer
-        cortex_m::register::msp::write_ns(nonsecure_sp);
+        // cortex_m::register::msp::write_ns(nonsecure_sp);
 
         rprintln!("Jumping");
+        rtt_target::UpChannel::conjure(0).unwrap().flush();
+
+        //[NSACR_CP0, NSACR_CP1, NSACR_CP10, NSACR_CP11, AHB_MISC_CTRL_REG_ENABLE_SECURE_CHECKING, AHB_MISC_CTRL_REG_WRITE_LOCK]
+
+        cortex_m::asm::dsb();
+        cortex_m::asm::isb();
+
+        ahb_secure_ctrl.apb_grp0_mem_rule0().write(|w| w.bits(0xFCFFFFFF));
+        ahb_secure_ctrl.apb_grp0_mem_rule1().write(|w| w.bits(0xCCFFFFFF));
+        ahb_secure_ctrl.apb_grp1_mem_rule0().write(|w| w.bits(0xFCCFFFFF));
+        ahb_secure_ctrl.apb_grp1_mem_rule1().write(|w| w.bits(0xCCCCCCCC));
+        ahb_secure_ctrl.apb_grp1_mem_rule2().write(|w| w.bits(0xFCFFFFFC));
+
+        ahb_secure_ctrl.ahb_periph0_slave_rule0().write(|w| w.bits(0xFCCCFCCC));
+        ahb_secure_ctrl.aips_bridge0_mem_rule0().write(|w| w.bits(0xFFFCCCCC));
+        ahb_secure_ctrl.ahb_periph1_slave_rule0().write(|w| w.bits(0xCCCCCCCC));
+        ahb_secure_ctrl.aips_bridge1_mem_rule0().write(|w| w.bits(0xCCFCFFFF));
+        ahb_secure_ctrl.aips_bridge1_mem_rule1().write(|w| w.bits(0xFFFFCCCC));
+        ahb_secure_ctrl.ahb_periph2_slave_rule0().write(|w| w.bits(0xFFFFCCCF));
+        ahb_secure_ctrl.ahb_periph3_slave_rule0().write(|w| w.bits(0xFFFCCFCC));
+
+        let scn_scb = &*ScnScb::ptr().add(0x1000_0000);
+        scn_scb.cppwr().modify(|_, w| w.bits(0));
+
+        // let scb = &*SCB::ptr();
+        // scb.shcsr.modify(|w| w & 0x0FFF7FFFF); // Disable bit 19 / SECUREFAULTENA
+        cp.SCB.shcsr.modify(|w| w | (1 << 19));
+
+        let ptr = __cortex_m_rt_HardFault_trampoline as *mut u32;
+        rprintln!(
+            "TT {:#010X}, {:#010X}, {:#010X}, {:#010X}, {:#010X}",
+            ptr as u32,
+            cortex_m::asm::tt(ptr),
+            cortex_m::asm::ttt(ptr),
+            cortex_m::asm::tta(ptr),
+            cortex_m::asm::ttat(ptr),
+        );
+        let ptr = 0x21000000 as *mut u32;
+        rprintln!(
+            "TT {:#010X}, {:#010X}, {:#010X}, {:#010X}, {:#010X}",
+            ptr as u32,
+            cortex_m::asm::tt(ptr),
+            cortex_m::asm::ttt(ptr),
+            cortex_m::asm::tta(ptr),
+            cortex_m::asm::ttat(ptr),
+        );
+
+        rprintln!("Locking master sec level");
+        ahb_secure_ctrl
+            .master_sec_level()
+            .modify(|_, w| w.master_sec_level_lock().blocked());
+        ahb_secure_ctrl
+            .master_sec_level_anti_pol()
+            .modify(|_, w| w.master_sec_level_anti_pole_lock().blocked());
+
+        rprintln!(
+            "master_sec_level: {:#010X}, {:#010X}",
+            ahb_secure_ctrl.master_sec_level().read().bits(),
+            ahb_secure_ctrl.master_sec_level_anti_pol().read().bits()
+        );
+
+        cortex_m::asm::dsb();
+        cortex_m::asm::isb();
+
+        // rprintln!("Test");
+        // __cortex_m_rt_HardFault();
+
+        rprintln!("Enabling AHB bus checks (1/2)");
+        rtt_target::UpChannel::conjure(0).unwrap().flush();
+
+        ahb_secure_ctrl
+            .misc_ctrl_reg()
+            .modify(|_, w| w.enable_secure_checking().enable());
+
+        cortex_m::asm::dsb();
+        cortex_m::asm::isb();
+        rprintln!("Enabling AHB bus checks (2/2)");
+        rtt_target::UpChannel::conjure(0).unwrap().flush();
+
+        ahb_secure_ctrl
+            .misc_ctrl_dp_reg()
+            .modify(|_, w| w.enable_secure_checking().enable());
+
+        rprintln!(
+            "misc_ctrl_reg: {:#010X}, {:#010X}",
+            ahb_secure_ctrl.misc_ctrl_reg().read().bits(),
+            ahb_secure_ctrl.misc_ctrl_dp_reg().read().bits()
+        );
 
         // Create the right function pointer to the reset vector
         let nonsecure_reset =
@@ -237,10 +345,14 @@ fn main() -> ! {
         // Prioritize and allow faults in the non-secure side
         // cp.SCB.aircr.modify(|w| w & !(1 << 14) & !(1 << 13));
 
-        // Jump
-        nonsecure_reset();
+        loop {
+            cortex_m::asm::nop();
+        }
 
-        cortex_m::asm::udf();
+        // Jump
+        // nonsecure_reset();
+
+        // cortex_m::asm::udf();
     }
 }
 
@@ -317,7 +429,16 @@ unsafe fn SecureFault() -> ! {
 
 #[cortex_m_rt::exception(trampoline = false)]
 unsafe fn HardFault() -> ! {
-    rprintln!("HardFault!");
+    let scb = &*cortex_m::peripheral::SCB::PTR;
+    rprintln!("HardFault! - HFSR: {:#010X}", scb.hfsr.read());
+    loop {
+        cortex_m::asm::nop();
+    }
+}
+
+#[interrupt]
+unsafe fn SECUREVIOLATION() {
+    rprintln!("SecureViolation!");
     loop {
         cortex_m::asm::nop();
     }
@@ -326,7 +447,9 @@ unsafe fn HardFault() -> ! {
 #[panic_handler]
 fn panic(i: &PanicInfo) -> ! {
     rprintln!("{}", i);
-    cortex_m::asm::udf();
+    loop {
+        cortex_m::asm::nop();
+    }
 }
 
 static COUNTER: AtomicU32 = AtomicU32::new(0);
